0743248db42fb82f0fbc6175aa8004e5630c461b
Paulus Gunadi <paulus@sg.ibm.com>
2018-01-29 16:26:20 +0800

    added image download for ee

--- scripts/common/download_installer.sh
+++ scripts/common/download_installer.sh
@@ -10,9 +10,9 @@ icp_source_path=$4
 icp_target_path=$5
 
 if [ -n "$icp_source_user" -a -n "$icp_source_password" -a -n "$icp_source_path" -a -n "$icp_target_path" ]; then
-  echo "Start download installation file"
+  echo "Start downloading installation file"
   python $BASEDIR/remote_copy.py $icp_source_server $icp_source_user $icp_source_password $icp_source_path $icp_target_path
-  echo "Complete download instalaltion file"
+  echo "Completed download installation file"
   echo "Start loading image to docker"
   tar xf $icp_target_path -O | sudo docker load && rm $icp_target_path
   echo "Finished loading image to docker"
