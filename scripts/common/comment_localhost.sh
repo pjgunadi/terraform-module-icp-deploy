779eff5fd2cb59989c5ff4ef236454ec6a9a9963
Paulus Gunadi <paulus@sg.ibm.com>
2019-02-27 23:54:52 +0800

    update localhost

--- scripts/common/comment_localhost.sh
+++ scripts/common/comment_localhost.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sudo sed -i "s/^127.0.0.1/#127.0.0.1/;s/^127.0.1.1/#127.0.1.1/" /etc/hosts
+sudo sed -i "s/^127.0.0.1.*$(hostname)/127.0.0.1 localhost/;s/^127.0.1.1/#127.0.1.1/" /etc/hosts
 sudo sed -i "s/^nameserver 127.0/#nameserver 127.0/" /etc/resolv.conf
